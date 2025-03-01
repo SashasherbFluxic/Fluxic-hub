b396e230516f520be8c6c4821d1718faa48ed40a
SashasherbFluxic <a23szm9@gmail.com>
2025-03-01 23:08:58 +0500

    Update and rename version.lua to Version.lua

---
+++ Version.lua
@@ -0,0 +1,6 @@
+return {
+    version = "1.0.0", 
+    update_url = "https://gist.githubusercontent.com/SashasherbFluxic/ca21bc9bcf22de3b8c16367acea9cf30/raw/d358eb7e8889c44332edc4694e5a27fe8133ce3a/Fluxic%2520hub.lua",
+    hash = "A1B2C3D4E5F6", 
+    force_update = false
+}
