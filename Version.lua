c48b5e8630deb82813ee6bd1cdfdd50420528c85
SashasherbFluxic <a23szm9@gmail.com>
2025-03-01 23:10:25 +0500

    Update Version.lua

--- Version.lua
+++ Version.lua
@@ -1,6 +1,6 @@
 return {
     version = "1.0.0", 
     update_url = "https://gist.githubusercontent.com/SashasherbFluxic/ca21bc9bcf22de3b8c16367acea9cf30/raw/d358eb7e8889c44332edc4694e5a27fe8133ce3a/Fluxic%2520hub.lua",
-    hash = "A1B2C3D4E5F6", 
+    hash = "c4316a46d93c428a6416fda0e37d81118f18ad2dfe4f4181a8e3b9af8818cfbd", 
     force_update = false
 }
