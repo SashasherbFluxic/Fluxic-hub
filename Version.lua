f1e372b0eeb24997fcc066b6e525c2d816d4332d
SashasherbFluxic <a23szm9@gmail.com>
2025-03-02 09:49:03 +0500

    Update Version.lua

--- Version.lua
+++ Version.lua
@@ -1,5 +1,5 @@
 return {
-    version = "1.0.0",
+    version = "3.1",
     script_url = "https://raw.githubusercontent.com/SashasherbFluxic/Fluxic-hub/main/Fluxic%20hub.lua",
     hash = "d0a1f2f4ac7aa52056f3f44854bdf729a99c6aef0607dcffae735a979d7014d6"
 }
